38aa8d0c17f3e4552602f2d5a309800b70576097
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-12-09 17:36:19 +0530

    Create superHeros.js

---
+++ app/models/superHeros.js
@@ -0,0 +1,37 @@
+exports.superHeros = [
+    {
+        "name": "Wade",
+        "superName": "Deadpool",
+        "profession": "Street fighter",
+        "age": 28,
+        "canFly": true
+    },
+    {
+        "name": "Bruce",
+        "superName": "Hulk",
+        "profession": "Doctore",
+        "age": 50,
+        "canFly": false
+    },
+    {
+        "name": "Steve",
+        "superName": "Captain America",
+        "profession": "Solder",
+        "age": 120,
+        "canFly": false
+    },
+    {
+        "name": "Tony",
+        "superName": "Iron Man",
+        "profession": "Business man",
+        "age": 45,
+        "canFly": true
+    },
+    {
+        "name": "Peter",
+        "superName": "Spider Man",
+        "profession": "Student",
+        "age": 21,
+        "canFly": true
+    }
+];
