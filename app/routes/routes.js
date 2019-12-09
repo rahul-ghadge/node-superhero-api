e6221147fed2111c0a2e424b31500858d5277c0f
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-12-09 17:37:26 +0530

    Create routes.js

---
+++ app/routes/routes.js
@@ -0,0 +1,19 @@
+
+module.exports = (app) => {
+    const superHeroController = require('../controllers/cotroller.js');
+
+    // Create a new super hero
+    app.post('/super-heros', superHeroController.create);
+
+    // Retrieve all super heros
+    app.get('/super-heros', superHeroController.findAll);
+
+    // Retrieve a single super hero
+    app.get('/super-heros/:name', superHeroController.findOne);
+
+    // Update a existing super hero
+    app.put('/super-heros/:name', superHeroController.update);
+
+    // Delete a super hero
+    app.delete('/super-heros/:name', superHeroController.delete);
+}
